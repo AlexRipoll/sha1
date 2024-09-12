cbdc908e99b7f3cee76d6df33414fdef25541b05
Alex Ripoll <alexripollpiera@gmail.com>
2024-09-12 12:11:20 +0200

    build w sequence

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,14 +1,22 @@
 pub struct Sha1;
 
+const H0: u32 = 0x67452301;
+const H1: u32 = 0xEFCDAB89;
+const H2: u32 = 0x98BADCFE;
+const H3: u32 = 0x10325476;
+const H4: u32 = 0xC3D2E1F0;
+
 impl Sha1 {
     pub fn new() -> Self {
         Self
     }
 
     pub fn hash(&mut self, key: &str) -> [u8; 20] {
-        let msg = self.pad_message(key);
-        println!("{:?}", msg);
+        let (mut h0, mut h1, mut h2, mut h3, mut h4) = (H0, H1, H2, H3, H4);
 
+        let msg = self.pad_message(key);
+
+        for chunk in msg.chunks(64) {}
         [0; 20]
     }
 
@@ -26,5 +34,25 @@ impl Sha1 {
         bytes.extend_from_slice(&bit_length.to_be_bytes());
 
         bytes
+    }
+
+    /// Builds W sequence array from a 512-bit chunk.
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc3174#section-6.1 a and b
+    fn build_w_sequence(&mut self, chunk: &[u8]) -> [u32; 80] {
+        let mut w = [0u32; 80];
+
+        // Initialize the first 16 words in the array from the chunk.
+        for (i, block) in chunk.chunks(4).enumerate() {
+            w[i] = u32::from_be_bytes(block.try_into().unwrap());
+        }
+
+        // Extend the schedule array using previously defined values and XOR (^) operations.
+        for i in 16..80 {
+            w[i] = w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16];
+            w[i] = w[i].rotate_left(1);
+        }
+
+        w
     }
 }
