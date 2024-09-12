d9a4591e35a1dc66c306393b07a27b85215ecfeb
Alex Ripoll <alexripollpiera@gmail.com>
2024-09-12 13:21:51 +0200

    digest to hex

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ mod sha1;
 
 fn main() {
     let mut sha1 = Sha1::new();
+    let hex: String = sha1
+        .digest("hello world")
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect();
+
+    println!("{}", hex);
 }
